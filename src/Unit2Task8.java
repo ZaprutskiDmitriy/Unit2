d540d4c9824e31b6e9a7661f9bfc9dcdfbf4bf33
ZaprutskiDmitriy <85133139+ZaprutskiDmitriy@users.noreply.github.com>
2021-06-01 22:02:22 +0300

    Add files via upload

--- src/Unit2Task8.java
+++ src/Unit2Task8.java
@@ -1,13 +1,19 @@
 public class Unit2Task8 {
     public static void main(String[] args) {
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<(i+1); j++){
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < (3 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int m = 0; m < (i + 1); m++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int k=0; k<4; k++) {
-            for (int l=k; l<4; l++){
+        for (int k = 0; k < 4; k++) {
+            for (int n = 0; n < k; n++) {
+                System.out.print(" ");
+            }
+            for (int l = k; l < 4; l++) {
                 System.out.print("*");
             }
             System.out.println();
